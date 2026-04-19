306689aa803d8bf6ee59ca1df0abad72229fb79e
lahlou <lachi.lahlou2023@gmail.com>
2026-04-19 17:45:25 -0400

    Ajout des champs information salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -5,3 +5,24 @@ class ViewSalle(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.service_salle = ServiceSalle()
+        self.frame_info = ctk.CTkFrame(self)
+        self.frame_info.pack(pady=10, padx=10, fill="x")
+        self.label_code = ctk.CTkLabel(self.frame_info, text="code salle")
+        self.label_code.grid(row=0, column=0, padx=10, pady=5)
+        self.entry_code = ctk.CTkEntry(self.frame_info)
+        self.entry_code.grid(row=0, column=1, padx=10, pady=5)
+
+        self.label_libelle = ctk.CtkLabel(self.frame_info, text="libelle")
+        self.label_libelle.grid(row=1, column=0, padx=10, pady=5)
+        self.entry_libelle = ctk.CTkEntry(self.frame_info)
+        self.entry_libelle.grid(row=1, column=1, padx=10, pady=5)
+
+        self.label_type = ctk.CTkLabel(self.frame_info, text="Type")
+        self.label_type.grid(row=2, column=0, padx=10, pady=5)
+        self.entry_type = ctk.CTkEntry(self.frame_info)
+        self.entry_type.grid(row=2, column=1, padx=10, pady=5)
+
+        self.label = ctk.CTkLabel(self.frame_info, text="Capacite")
+        self.label.grid(row=3, column=0, padx=10, pady=5)
+        self.entry_capacite = ctk.CTkEntry(self.frame_info)
+        self.entry_capacite.grid(row=3, column=1, padx=10, pady=5)
