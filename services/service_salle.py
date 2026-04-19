2e66a9726073b7ff2da27825f2e2174d3176a3fc
lahlou <lachi.lahlou2023@gmail.com>
2026-04-19 04:22:23 -0400

    Ajout de la methode ajouter_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -10,3 +10,13 @@ class ServiceSalle:
             self.dao_salle.insert_salle(salle)
             return True
         return False
+    def ajouter_salle(self, salle):
+        if salle.code and salle.libelle and salle.type and salle.capacite >= 1:
+            self.dao_salle.insert_salle(salle)
+            return True, "Salle ajouter avec succes"
+        return False, "Erreur lors de l'ajout de la salle"
+    def modifier_salle(self, salle):
+        if salle.code and salle.libelle and salle.type and salle.capacite >= 1:
+            self.dao_salle.update_salle(salle)
+            return True, "Salle modifiee avec succes"
+        return False, "Erreur lors de la modification"
