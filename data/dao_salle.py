75f0baaf12add6e86a57563a382750f03688ed6d
lahlou <lachi.lahlou2023@gmail.com>
2026-04-18 22:51:51 -0400

    Ajout de la methode update_salle

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -40,6 +40,26 @@ def delete_salle(self, code):
     connection.commit()
     cursor.close()
     connection.close()
+def get_salle(self, code):
+    connection = self.get_connection()
+    cursor = connection.cursor()
+    requete = "SELECT * FROM salle WHERE code=%s"
+    cursor.execute(requete, (code,))
+    resultat = cursor.fetchone()
+    cursor.close()
+    connection.close()
+    if resultat:
+        return resultat
+    return None
+def get_salles(self):
+    connection = self.get_connection()
+    cursor = connection.cursor()
+    requete = "SELECT * FROM salle"
+    cursor.execute(requete)
+    resultats = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return resultats
 
 
 
