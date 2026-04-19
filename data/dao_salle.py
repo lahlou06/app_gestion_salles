48e197d90359f8220c44fd323b60405180a08dcf
lahlou <lachi.lahlou2023@gmail.com>
2026-04-18 22:20:08 -0400

    Ajout de la methode insert_salle

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -12,4 +12,15 @@ def get_connection(self):
         database=config["database"],
     )
     return connection
+def insert_salle(self,salle):
+    connection = self.get_connection()
+    cursor = connection.cursor()
+    requete = "INSERT INTO salle (code, libelle, type, capacite) VALUES (%s, %s, %s, %s)"
+    valeurs = (salle.code, salle.libelle, salle.type, salle.capacite)
+    cursor.excute(requete, valeurs)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
 
