655ff0e463c9210e26fac9f5e70b93b746a1da83
lahlou <lachi.lahlou2023@gmail.com>
2026-04-19 19:49:32 -0400

    creation de classe salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,78 +1,16 @@
 import customtkinter as ctk
-import tkinter.ttk as ttk
-from services.service_salle import ServiceSalle
-from models.salle import Salle
+from tkinter import ttk, messagebox
 
+from models.salle import Salle
+from services.service_salle import ServiceSalle
 
 class ViewSalle(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-        self.table.pack()
-
-        def ajouter_salle(self):
-            code = self.entry_code.get()
-            libelle = self.entry_libelle.get()
-            type_salle = self.entry_type.get()
-            capacite = int(self.entry_capacite.get())
-
-            salle = Salle(code, libelle, type_salle, capacite)
-            self.service_salle.ajouter_salle(salle)
-
-        self.frame_liste = ctk.CTkFrame(self)
-        self.frame_liste.pack(pady=10)
-
-        self.table = ttk.Treeview(
-            self.frame_liste,
-            columns=("code", "libelle", "type", "capacite"),
-            show="headings"
-        )
-
-        self.table.heading("code", text="Code")
-        self.table.heading("libelle", text="Libelle")
-        self.table.heading("type", text="Type")
-        self.table.heading("capacite", text="Capacite")
-
-        self.table.column("code", width=80)
-        self.table.column("libelle", width=150)
-        self.table.column("type", width=120)
-        self.table.column("capacite", width=100)
-
-        self.table.pack()
+    def _init_(self):
+        super()._init_()
+        self.title("Gestion des salles")
+        self.geometry("750x500")
 
         self.service_salle = ServiceSalle()
-        self.frame_info = ctk.CTkFrame(self)
-        self.frame_info.pack(pady=10, padx=10, fill="x")
-        self.label_code = ctk.CTkLabel(self.frame_info, text="code salle")
-        self.label_code.grid(row=0, column=0, padx=10, pady=5)
-        self.entry_code = ctk.CTkEntry(self.frame_info)
-        self.entry_code.grid(row=0, column=1, padx=10, pady=5)
 
-        self.label_libelle = ctk.CtkLabel(self.frame_info, text="libelle")
-        self.label_libelle.grid(row=1, column=0, padx=10, pady=5)
-        self.entry_libelle = ctk.CTkEntry(self.frame_info)
-        self.entry_libelle.grid(row=1, column=1, padx=10, pady=5)
-
-        self.label_type = ctk.CTkLabel(self.frame_info, text="Type")
-        self.label_type.grid(row=2, column=0, padx=10, pady=5)
-        self.entry_type = ctk.CTkEntry(self.frame_info)
-        self.entry_type.grid(row=2, column=1, padx=10, pady=5)
-
-        self.label = ctk.CTkLabel(self.frame_info, text="Capacite")
-        self.label.grid(row=3, column=0, padx=10, pady=5)
-        self.entry_capacite = ctk.CTkEntry(self.frame_info)
-        self.entry_capacite.grid(row=3, column=1, padx=10, pady=5)
-
-        self.frame_actions = ctk.CTkFrame(self)
-        self.frame_actions.pack(pady=10)
-
-        self.btn_ajouter = ctk.CTkButton(self.frame_actions, text="ajouter", command=self.ajouter_salle)
-        self.btn_ajouter.grid(row=0, column=0, padx=10)
-
-        self.btn_modifier = ctk.CtkButton(self.frame_actions, text="modifier")
-        self.btn_modifier.grid(row=0, column=1, padx=10)
-
-        self.btn_supprimer = ctk.CtkButton(self.frame_actions, text="supprimer")
-        self.btn_supprimer.grid(row=0, column=2, padx=10)
-
-        self.btn_rechercher = ctk.CTkButton(self.frame_actions, text="rechercher")
-        self.btn_rechercher.grid(row=0, column=3, padx=10)
+        self.creer_widgets()
+        self.lister_salles()
