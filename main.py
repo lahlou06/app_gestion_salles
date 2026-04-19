a06113019d5eccd8f8249ea46e71b5983ef1806b
lahlou <lachi.lahlou2023@gmail.com>
2026-04-19 03:23:47 -0400

    je vien d'ajouter une salle

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ connexion = dao.get_connection()
 if connexion:
     print("connexion reussie")
     connexion.close()
+from data.dao_salle import DataSalle
+from models.salle import Salle
+dao = DataSalle()
+salle = Salle("A06", "Salle Info", "Laboratoire", 30)
+dao.insert_salle(salle)
+print("salle ajoutee")
